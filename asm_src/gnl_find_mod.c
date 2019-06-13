30c6a81c63c254c27bef0de2f21d5718f7a9ac27
Mathieu GINISTY <maginist@e3r11p10.42.fr>
2019-06-13 15:02:28 +0200

    add fct comment

--- asm_src/gnl_find_mod.c
+++ asm_src/gnl_find_mod.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   gnl_find_mod.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: floblanc <floblanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maginist <maginist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 17:37:00 by floblanc          #+#    #+#             */
-/*   Updated: 2019/06/13 14:38:46 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:00:55 by maginist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../includes/op.h"
 
-void    name_stocker(char **line, int *i, t_data **start, int ret)
+
+void   comment_stocker(char **line, int *i, t_data **start, int ret)
 {
     static int     diff;
 
@@ -20,7 +21,27 @@ void    name_stocker(char **line, int *i, t_data **start, int ret)
         diff = 4 - i;
     while ((*line)[*i] && (*line)[*i] != '"')
     {
-        (*start)->str[diff + i] = (*line)[*i];
+        ((*start)->next)->str[diff + i] = (*line)[*i];
+        (*i)++;
+    }
+    if (ret == 1 && (!(*line)[*i]))
+    {
+        ((*start)->next)->str[diff + (*i)] = '\n';
+        diff += (*i) + 1;
+    }
+    else
+        diff += *i;
+}
+
+void    name_stocker(char **line, int *i, t_data **start, int ret)
+{
+    static int     diff;
+
+    if (!(diff))
+        diff = 4 - (*i);
+    while ((*line)[*i] && (*line)[*i] != '"')
+    {
+        (*start)->str[diff + (*i)] = (*line)[*i];
         (*i)++;
     }
     if (ret == 1 && (!(*line)[*i]))
@@ -31,14 +52,18 @@ void    name_stocker(char **line, int *i, t_data **start, int ret)
     else
         diff += *i;
 }
-int     gnl_find_mod(char **line, t_data **start, int *reader)
+
+int     gnl_find_mod(char **line, t_data **start, int *reader, char c_or_n)
 {
     int i;
 
     while ((reader[1] = get_next_line_mod(reader[0], line)) > 0)
     {
         i = 0;
-        name_stocker(line, &i, start, reader[1]);
+        if (c_or_n == "n")
+            name_stocker(line, &i, start, reader[1]);
+        else if (c_or_n == "c")
+            comment_stocker(line, &i, start, reader);
         if ((*line)[i] == '"')
             return (1);
         ft_strdel(line);
--- asm_src/parsing_stock_first_half.c
+++ asm_src/parsing_stock_first_half.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_stock_first_half.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: floblanc <floblanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maginist <maginist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 14:38:17 by floblanc          #+#    #+#             */
-/*   Updated: 2019/06/13 14:38:51 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:00:07 by maginist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/op.h"
+
+void     is_comment(char **line, t_data **start, int *reader, int *step)
+{
+    int i;
+
+    i = 0;
+    while (((*line)[i] && (*line)[i] <= ' ') || (*line)[i] == 127)
+        i++;
+    if (ft_strncmp(line + i, COMMENT_CMD_STRING, 8) != 0)
+        return ;
+    i += 5;
+    while (((*line)[i] && (*line)[i] <= ' ') || (*line)[i] == 127)
+        i++;
+    if (!((*line)[i]) || (*line)[i] != '"')
+        return ;
+    i++;
+    comment_stocker(line, &i, start, reader[1]);
+    if (!((*line)[i]) && reader[1] == 1)
+    {
+        if (!(gnl_find_mod(line, start, reader, "c")))
+            return ;
+    }
+    while (((*line)[i] && (*line)[i] <= ' ') || (*line)[i] == 127)
+        i++;
+    if (!(line[i]))
+        *step = 1;
+}
 
 void     is_name(char **line, t_data **start, int *reader, int *step)
 {
@@ -30,7 +57,7 @@ void     is_name(char **line, t_data **start, int *reader, int *step)
     name_stocker(line, &i, start, reader[1]);
     if (!((*line)[i]) && reader[1] == 1)
     {
-        if (!(gnl_find_mod(line, start, reader))
+        if (!(gnl_find_mod(line, start, reader, "n")))
             return ;
     }
     while (((*line)[i] && (*line)[i] <= ' ') || (*line)[i] == 127)
@@ -44,20 +71,20 @@ void    init_cor(t_data **start)
     t_data  *comment;
     t_data  *command;
 
-    if (!((*start) = (t_data*)malloc(sizeof(t_data) * 1))
+    if (!((*start) = (t_data*)malloc(sizeof(t_data) * 1)))
             return ;
     ft_bzero((*start)->str, 4 + PROG_NAME_LENGTH);
     (*start)->str[1] = 234;
     (*start)->str[2] = 131;
     (*start)->str[3] = 243;
     (*start)->size = 4 + PROG_NAME_LENGTH;
-    (*start)->index = 1
-    if (!(comment = (t_data*)malloc(sizeof(t_data) * 1))
-            return ;
+    (*start)->index = 1;
+    if (!(comment = (t_data*)malloc(sizeof(t_data) * 1)))
+        return ;
     ft_bzero(comment->str, 12 + COMMENT_LENGTH);
     comment->size = 12 + COMMENT_LENGTH;
     comment->index = 2;
-    if (!(command = (t_data*)malloc(sizeof(t_data) * 1))
+    if (!(command = (t_data*)malloc(sizeof(t_data) * 1)))
             return ;
     ft_bzero(command->str, CHAMP_MAX_SIZE);
     command->size = CHAMP_MAX_SIZE;
@@ -73,15 +100,15 @@ int     line_is_correct(char **line, t_data **start, t_label **lab, int *reader)
 
     if (!(*line))
         return (1);
-    if (!(step)
+    if (!(step))
     {
         step = 0;
         init_cor(start);
     }
     if (step == 0)
         is_name(line, start, reader, &step);
-    else if (is_comment(*line) && step == 1)
-        stock_comment(start, *line, &step);
+    else if (step == 1)
+        is_comment(*line, start, reader, &step);
     else if (is_command(*line) && step == 2)
         stock_command(start, *line, lab);
     else if (line[0] != COMMENT_CHAR && line[0] != ';')
