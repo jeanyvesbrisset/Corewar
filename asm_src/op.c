266e5e3a21fc2b04b675119d81883ede24d9870c
Florian Blanchard <floblanc@MacBook-Pro-de-Florian.local>
2019-06-22 00:46:14 +0200

    note des sizes de chaque commandes a reverifier pour etre sur mais ca devrait etre bon

--- asm_src/op.c
+++ asm_src/op.c
@@ -6,7 +6,7 @@
 /*   By: floblanc <floblanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 10:58:57 by maginist          #+#    #+#             */
-/*   Updated: 2019/06/21 16:18:01 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/06/22 00:45:26 by floblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,27 +35,27 @@ t_f		g_f_tab[17] =
 
 t_op	g_op_tab[17] =
 {
-	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 0},
-	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0},
-	{"st", 2, {T_REG, T_IND | T_REG}, 3, 5, "store", 1, 0},
-	{"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1, 0},
-	{"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction", 1, 0},
+	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 0}, //size en octets : 5
+	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0}, //size en octets : 5-7
+	{"st", 2, {T_REG, T_IND | T_REG}, 3, 5, "store", 1, 0}, //size en octets : 4-5
+	{"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1, 0}, //size en octets : 5
+	{"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction", 1, 0}, //size en octets : 5
 	{"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 6,
-		"et (and  r1, r2, r3   r1&r2 -> r3", 1, 0},
+		"et (and  r1, r2, r3   r1&r2 -> r3", 1, 0}, //size en octets : 5-11
 	{"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 6,
-		"ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0},
+		"ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0}, //size en octets : 5-11
 	{"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6,
-		"ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0},
-	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1},
+		"ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0}, //size en octets : 5-11
+	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1}, //size en octets : 3
 	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25,
-		"load index", 1, 1},
+		"load index", 1, 1}, //size en octets : 5-7
 	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25,
-		"store index", 1, 1},
-	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1},
-	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0},
+		"store index", 1, 1}, //size en octets : 5-7
+	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1}, //size en octets : 3
+	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0}, //size en octets : 5-7
 	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50,
-		"long load index", 1, 1},
-	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},
-	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0},
+		"long load index", 1, 1}, //size en octets : 5-7
+	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1}, //size en octets : 3
+	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0}, //size en octets : 3
 	{0, 0, {0}, 0, 0, 0, 0, 0}
 };
